c75134680a05a453f2514dc7603f50fb1294afab
ISNing <33572057+ISNing@users.noreply.github.com>
2025-08-11 16:51:30 +0800

    Update k8s registry host

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const routes = {
   ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
   ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
   ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
+  ["k8s-registry." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
   ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
   ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
   ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
